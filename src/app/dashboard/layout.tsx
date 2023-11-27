07bb2dcd59497580d12a816fa22dc0c2d78172d8
Elivelton Santos <samsepi0l.dev@gmail.com>
2023-11-27 03:03:55 -0300

    refactor: add media querys

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout ({ children }: DashboardLayoutProps) {
   return (
-    <main className='w-full h-screen bg-white md:px-56 px-10'>
+    <main className='w-full h-screen bg-white max-md:px-10 max-lg:px-16 max-xl:px-36 px-56'>
       <Header />
       <div>
         {children}
