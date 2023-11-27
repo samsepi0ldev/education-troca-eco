06c456922a64226a9e12db13248749008d95badc
Elivelton Santos <samsepi0l.dev@gmail.com>
2023-11-27 01:08:54 -0300

    feat create benefits page

---
+++ src/app/dashboard/beneficios/page.tsx
@@ -0,0 +1,11 @@
+import { Navigation } from '@/components/navigation'
+
+export default function Benefits () {
+  return (
+    <div className='pt-5'>
+      <div className='w-fit rounded-lg bg-zinc-100 px-4 py-2.5 flex items-center justify-center gap-2 text-zinc-500'>
+        <Navigation />
+      </div>
+    </div>
+  )
+}
