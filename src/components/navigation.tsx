69da6d0f649f6a557c6b8ded1cf02a4bca0e9a5b
Elivelton Santos <samsepi0l.dev@gmail.com>
2023-11-27 02:36:37 -0300

    refactor: add div in navigation to bg styling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -7,13 +7,13 @@ export function Navigation () {
   const pathnames = usePathname().split('/')
   pathnames.shift()
   return (
-    <>
+    <div className='w-fit rounded-lg bg-zinc-100 px-4 py-2.5 flex items-center justify-center gap-2 text-zinc-500'>
       {pathnames.map(path => (
           <div className='flex items-center justify-center gap-2 [&>svg]:last:hidden' key={path}>
             <span className='capitalize text-xs'>{path}</span>
             <ChevronRight strokeWidth={2} size={20} />
           </div>
         ))}
-    </>
+    </div>
   )
 }
